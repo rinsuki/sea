93289b2e767f7831cfbc7b8349d5e175b5d24a73
rinsuki <428rinsuki+git@gmail.com>
2019-05-01 02:10:42 +0900

    タイムラインができた

--- src/routers/api/v1/timelines.ts
+++ src/routers/api/v1/timelines.ts
@@ -1,5 +1,34 @@
 import { APIRouter } from "../router-class"
+import { getRepository } from "typeorm"
+import { Post } from "../../../db/entities/post"
+import { PostRepository } from "../../../db/repositories/post"
+import $ from "cafy"
 
 const router = new APIRouter()
+
+router.get("/public", async ctx => {
+    const queryBefore = $.obj({
+        count: $.str.match(/^[0-9]+$/).makeOptional(),
+    }).throw(ctx.query)
+    const query = $.obj({
+        count: $.num
+            .int()
+            .range(1, 100)
+            .makeOptional(),
+    }).throw({
+        count:
+            queryBefore.count == null ? undefined : parseInt(queryBefore.count),
+    })
+    await ctx.sendMany(
+        PostRepository,
+        await getRepository(Post)
+            .createQueryBuilder("post")
+            .leftJoinAndSelect("post.user", "users")
+            .leftJoinAndSelect("post.application", "applications")
+            .limit(query.count || 20)
+            .orderBy("post.createdAt", "DESC")
+            .getMany()
+    )
+})
 
 export default router
