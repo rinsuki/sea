55f5abce83fcf921272b5447bb49b478e67cdbf5
rinsuki <428rinsuki+git@gmail.com>
2019-05-09 02:49:32 +0900

    implement PATCH /api/v1/account

--- src/routers/api/v1/account.ts
+++ src/routers/api/v1/account.ts
@@ -1,10 +1,29 @@
 import { APIRouter } from "../router-class"
 import { UserRepository } from "../../../db/repositories/user"
+import koaBody = require("koa-body")
+import $ from "cafy"
+import { getRepository } from "typeorm"
+import { User } from "../../../db/entities/user"
 
 const router = new APIRouter()
 
-router.get("/verify_credentials", async ctx => {
+router.get("/", async ctx => {
     await ctx.send(UserRepository, ctx.state.token.user)
+})
+router.redirect("/verify_credentials", "/api/v1/account", 301)
+
+router.patch("/", koaBody(), async ctx => {
+    const body = $.obj({
+        name: $.str
+            .makeOptional()
+            .min(1)
+            .max(20),
+    }).throw(ctx.request.body)
+
+    const user = ctx.state.token.user
+    if (body.name != null) user.name = body.name
+    await getRepository(User).save(user)
+    await ctx.send(UserRepository, user)
 })
 
 export default router
