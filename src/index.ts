60514f99f3bf86592c32c5cf9df13b32fd61094d
rinsuki <428rinsuki+git@gmail.com>
2019-05-31 15:12:58 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,8 @@
-import "reflect-metadata"
-import Koa from "koa"
-import { createConnection, getConnectionOptions } from "typeorm"
-import webRouter from "./routers/web"
-import apiRouter from "./routers/api"
-import Router from "koa-router"
-import { isProductionMode, FORCE_HTTPS } from "./config"
-import WebSocket from "ws"
-import http from "http"
-import { streamingConnectionCallback } from "./routers/streaming"
+import server, { databaseSetup } from "./app"
 
-const app = new Koa()
-if (FORCE_HTTPS)
-    app.use(async (ctx, next) => {
-        if (ctx.request.headers["x-forwarded-proto"] !== "https") {
-            const url = ctx.request.URL
-            url.protocol = "https"
-            ctx.redirect(url.toString())
-            return
-        }
-        await next()
-    })
-const server = http.createServer(app.callback())
-const ws = new WebSocket.Server({ server })
-ws.addListener("connection", streamingConnectionCallback)
-
-const router = new Router<any, any>()
-router.use("/api", apiRouter.routes())
-router.use(webRouter.routes())
-app.use(router.routes())
-
-async function run() {
-    const config = await getConnectionOptions()
-    await createConnection({
-        ...config,
-        logging: isProductionMode ? [] : ["query"],
-    })
+databaseSetup().then(() => {
     const port = process.env.PORT || 3000
     server.listen(port, () => {
         console.log(`live in http://localhost:${port}`)
     })
-}
-
-run()
+})
