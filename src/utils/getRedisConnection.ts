f7fefd27858147eee7f91ebbed29ba02e5aa0318
rinsuki <428rinsuki+git@gmail.com>
2019-06-01 03:44:49 +0900

    型が付いてなかった

--- src/utils/getRedisConnection.ts
+++ src/utils/getRedisConnection.ts
@@ -2,7 +2,7 @@ import Redis from "redis"
 import { REDIS_URL } from "../config"
 
 export function getRedisConnection() {
-    if (process.env.NODE_ENV === "test") return require("redis-mock").createClient()
+    if (process.env.NODE_ENV === "test") return require("redis-mock").createClient() as Redis.RedisClient
     return Redis.createClient(REDIS_URL)
 }
 
