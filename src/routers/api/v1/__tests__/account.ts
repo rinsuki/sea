2f1eca5e692ea0e25e81e431da44c9fd1c32cea9
rinsuki <428rinsuki+git@gmail.com>
2019-06-01 03:54:02 +0900

    fox

--- src/routers/api/v1/__tests__/account.ts
+++ src/routers/api/v1/__tests__/account.ts
@@ -3,6 +3,6 @@ import { request } from "../../../../utils/forTesting/testRequest"
 describe("/api/v1/account", () => {
     test("認証がないとコケる", () =>
         request(r => {
-            return r.get("/api/v1/account").expect(503)
+            return r.get("/api/v1/account").expect(400)
         }))
 })
