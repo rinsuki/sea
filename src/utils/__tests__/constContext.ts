e13e7a92c50cffe33a355db9e530305d8312eacc
rinsuki <428rinsuki+git@gmail.com>
2019-05-31 17:42:34 +0900

    fix

--- src/utils/__tests__/constContext.ts
+++ src/utils/__tests__/constContext.ts
@@ -1,14 +1,31 @@
-import $ from "cafy"
+import $, { Context } from "cafy"
 import { ConstContext } from "../constContext"
 
 describe("stringのとき", () => {
-    test("同じなら通る", () => {
-        expect($.type(ConstContext("test")).throw("test")).toEqual("test")
-    })
-
-    test("違うと通らない", () => {
-        expect(() => $.type(ConstContext("test")).throw("fuga")).toThrow()
-    })
+    // normal, undefined, null, invalid
+    const validValue = "valid"
+    const invalidValue = "invalid"
+    const values = [validValue, undefined, null, invalidValue]
+    const checkTable = [
+        ["normal", $.type(ConstContext(validValue)), [true, false, false, false]],
+        ["optional", $.type(ConstContext(validValue)).makeOptional(), [true, true, false, false]],
+        ["nullable", $.type(ConstContext(validValue)).makeNullable(), [true, false, true, false]],
+        ["optionalNullable", $.type(ConstContext(validValue)).makeOptionalNullable(), [true, true, true, false]],
+    ] as [string, Context, [boolean, boolean, boolean, boolean]][]
+    for (const v of checkTable) {
+        describe(v[0], () => {
+            for (const [i, n] of values.entries()) {
+                const flag = v[2][i]
+                test("" + n, () => {
+                    if (flag) {
+                        expect(v[1].throw(n)).toEqual(n)
+                    } else {
+                        expect(() => v[1].throw(n)).toThrow()
+                    }
+                })
+            }
+        })
+    }
 })
 
 // WIP: TypeScriptの型チェックしたい
