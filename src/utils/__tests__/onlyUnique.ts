6bd1a39c56f7b4d8eb6f2d1dd179bfae14893622
rinsuki <428rinsuki+git@gmail.com>
2019-05-31 18:51:51 +0900

    add some test cases

--- src/utils/__tests__/onlyUnique.ts
+++ src/utils/__tests__/onlyUnique.ts
@@ -5,4 +5,13 @@ describe("stringのとき", () => {
         const arr = [{ name: "hoge" }, { name: "fuga" }, { name: "piyo" }]
         expect(onlyUnique(arr, "name")).toStrictEqual(arr)
     })
+    test("被ってるやつがあったら最初のだけ残る", () => {
+        expect(
+            onlyUnique([{ name: "hoge", score: 1 }, { name: "hoge", score: 2 }, { name: "fuga", score: 3 }], "name")
+        ).toStrictEqual([{ name: "hoge", score: 1 }, { name: "fuga", score: 3 }])
+    })
+    test("keyじゃないところが被っても関与しない", () => {
+        const arr = [{ name: "hoge", type: "human" }, { name: "fuga", type: "human" }, { name: "piyo", type: "bot" }]
+        expect(onlyUnique(arr, "name")).toStrictEqual(arr)
+    })
 })
