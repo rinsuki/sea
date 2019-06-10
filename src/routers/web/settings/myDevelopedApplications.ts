887bdbaa8bacb89b3aac65e2aa0ae2923a4f300e
96a86c <96a86c@googlegroups.com>
2019-06-10 17:56:29 +0900

    optimization for HTMLForm

--- src/routers/web/settings/myDevelopedApplications.ts
+++ src/routers/web/settings/myDevelopedApplications.ts
@@ -60,22 +60,19 @@ router.post("/:id", koaBody(), checkCsrf, async ctx => {
         id: $.str.match(/^[0-9]+$/),
     }).throw(ctx.params)
     const body = $.obj({
-        name: $.str
-            .min(1)
-            .max(32)
-            .makeOptional(),
-        description: $.str.min(1).makeOptional(),
-        redirect_uri: $.str.makeOptional(),
-        is_automated: $.str.or("0|1").makeOptional(),
+        name: $.str.min(1).max(32),
+        description: $.str.min(1),
+        redirect_uri: $.str,
+        is_automated: $.str.or("1").makeOptional(),
     }).throw(ctx.request.body)
     const app = await getRepository(Application).findOneOrFail(id, {
         relations: ["ownerUser"],
     })
     if (app.ownerUser.id != ctx.state.session!.user.id) return ctx.throw(403, "お前ownerじゃねえだろ")
-    if (body.name != null) app.name = body.name
-    if (body.description != null) app.description = body.description
-    if (body.redirect_uri != null) app.redirectUri = body.redirect_uri
-    if (body.is_automated != null) app.isAutomated = Boolean(parseInt(body.is_automated))
+    app.name = body.name
+    app.description = body.description
+    app.redirectUri = body.redirect_uri
+    app.isAutomated = body.is_automated != undefined ? true : false
     await getRepository(Application).save(app)
     ctx.redirect("/settings/my_developed_applications/" + app.id)
 })
