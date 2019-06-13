0fda3bd155715c5b1b4504e9e008f67c29ba177c
96a86c <96a86c@googlegroups.com>
2019-06-13 21:40:26 +0900

    add DELETE method to subscriptions

--- src/db/repositories/subscription.ts
+++ src/db/repositories/subscription.ts
@@ -1,5 +1,6 @@
-import { EntityRepository, Repository } from "typeorm"
+import { EntityRepository, Repository, getCustomRepository } from "typeorm"
 import { Subscription } from "../entities/subscription"
+import { ApplicationRepository } from "./application"
 
 @EntityRepository(Subscription)
 export class SubscriptionRepository extends Repository<Subscription> {
@@ -8,13 +9,18 @@ export class SubscriptionRepository extends Repository<Subscription> {
     }
 
     async packMany(subscriptions: Subscription[]) {
-        return subscriptions.map(subscription => ({
-            id: subscription.id,
-            name: subscription.description,
-            endpoint: subscription.endpoint,
-            failedAt: subscription.failedAt,
-            createdAt: subscription.createdAt,
-            updatedAt: subscription.updatedAt,
-        }))
+        return Promise.all(
+            subscriptions.map(async subscription => {
+                return {
+                    id: subscription.id,
+                    name: subscription.description,
+                    endpoint: subscription.endpoint,
+                    failedAt: subscription.failedAt,
+                    application: await getCustomRepository(ApplicationRepository).pack(subscription.application),
+                    createdAt: subscription.createdAt,
+                    updatedAt: subscription.updatedAt,
+                }
+            })
+        )
     }
 }
--- src/routers/api/v1/webpush.ts
+++ src/routers/api/v1/webpush.ts
@@ -1,8 +1,8 @@
 import { APIRouter } from "../router-class"
 import koaBody = require("koa-body")
 import $ = require("transform-ts")
-import { $length } from "../../../utils/transformers"
-import { getRepository, getCustomRepository } from "typeorm"
+import { $length, $regexp, $stringNumber, $safeNumber, $literal } from "../../../utils/transformers"
+import { getRepository } from "typeorm"
 import { Subscription } from "../../../db/entities/subscription"
 import { WP_OPTIONS } from "../../../config"
 import { SubscriptionRepository } from "../../../db/repositories/subscription"
@@ -18,11 +18,13 @@ router.get("/server_key", async ctx => {
 
 router.get("/subscriptions", async ctx => {
     const subscriptions = await getRepository(Subscription).find({
-        user: ctx.state.token.user,
-        application: ctx.state.token.application,
-        revokedAt: null,
+        where: {
+            user: ctx.state.token.user,
+            revokedAt: null,
+        },
+        relations: ["application"],
     })
-    ctx.sendMany(SubscriptionRepository, subscriptions)
+    await ctx.sendMany(SubscriptionRepository, subscriptions)
 })
 
 router.post("/subscriptions", koaBody(), async ctx => {
@@ -43,6 +45,20 @@ router.post("/subscriptions", koaBody(), async ctx => {
     if (body.description != null) subscription.description = body.description
     await getRepository(Subscription).save(subscription)
     await ctx.send(SubscriptionRepository, subscription)
+})
+
+router.delete("/subscriptions/:id", async ctx => {
+    const { id } = $.obj({
+        id: $stringNumber.compose($safeNumber),
+    }).transformOrThrow(ctx.params)
+    const subscription = await getRepository(Subscription).findOneOrFail({
+        id: id,
+        user: ctx.state.token.user,
+        revokedAt: null,
+    })
+    await getRepository(Subscription).update({ id: subscription.id }, { revokedAt: new Date() })
+    ctx.body = JSON.stringify({ id: subscription.id })
+    ctx.type = "json"
 })
 
 export default router
