2850c22cdd754208590e9925a639347b29e6fc40
kokoro <motitaiyaki@users.noreply.github.com>
2019-06-14 16:22:07 +0900

    Add webpush (#42) (#48)
    * add webpush test
    
    * subscription revoke
    
    * fix payload
    
    * must web-push
    
    * use transform-ts instead cafy
    
    * change endpoint response and error handle
    
    * fix bug and add icon to push
    
    * use queryRunner
    
    * Update src/routers/api/v1/subscriptions.ts
    
    Co-Authored-By: rinsuki <428rinsuki+git@gmail.com>
    
    * change endpoint
    
    * use application context and fix column type
    
    * allow push failure until 3 times
    
    * fix migration file
    
    * allow push failure until a week
    
    * add DELETE method to subscriptions
    
    * change webpush payload
    
    * refactor
    
    * rewrite with queryBuilder and refactor
    
    * return 204
    
    * remove promiseall
    
    * use try-catch
    
    * add application context

---
+++ src/routers/api/v1/webpush.ts
@@ -0,0 +1,64 @@
+import { APIRouter } from "../router-class"
+import koaBody = require("koa-body")
+import $ = require("transform-ts")
+import { $length, $regexp, $stringNumber, $safeNumber, $literal } from "../../../utils/transformers"
+import { getRepository } from "typeorm"
+import { Subscription } from "../../../db/entities/subscription"
+import { WP_OPTIONS } from "../../../config"
+import { SubscriptionRepository } from "../../../db/repositories/subscription"
+
+const router = new APIRouter()
+
+router.get("/server_key", async ctx => {
+    ctx.type = "json"
+    ctx.body = JSON.stringify({
+        applicationServerKey: WP_OPTIONS.vapidDetails!.publicKey,
+    })
+})
+
+router.get("/subscriptions", async ctx => {
+    const subscriptions = await getRepository(Subscription).find({
+        where: {
+            user: ctx.state.token.user,
+            application: ctx.state.token.application,
+            revokedAt: null,
+        },
+    })
+    await ctx.sendMany(SubscriptionRepository, subscriptions)
+})
+
+router.post("/subscriptions", koaBody(), async ctx => {
+    const body = $.obj({
+        endpoint: $.string.compose($length({ min: 1 })),
+        keys: $.obj({
+            p256dh: $.string.compose($length({ min: 1 })),
+            auth: $.string.compose($length({ min: 1 })),
+        }),
+        description: $.optional($.string),
+    }).transformOrThrow(ctx.request.body)
+    const subscription = new Subscription()
+    subscription.user = ctx.state.token.user
+    subscription.application = ctx.state.token.application
+    subscription.endpoint = body.endpoint
+    subscription.publicKey = body.keys.p256dh
+    subscription.authenticationSecret = body.keys.auth
+    if (body.description != null) subscription.description = body.description
+    await getRepository(Subscription).save(subscription)
+    await ctx.send(SubscriptionRepository, subscription)
+})
+
+router.delete("/subscriptions/:id", async ctx => {
+    const { id } = $.obj({
+        id: $stringNumber.compose($safeNumber),
+    }).transformOrThrow(ctx.params)
+    const subscription = await getRepository(Subscription).findOneOrFail({
+        id: id,
+        user: ctx.state.token.user,
+        application: ctx.state.token.application,
+        revokedAt: null,
+    })
+    await getRepository(Subscription).update({ id: subscription.id }, { revokedAt: new Date() })
+    ctx.status = 204
+})
+
+export default router
