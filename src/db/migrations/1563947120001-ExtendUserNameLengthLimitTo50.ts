261dab2ee382014d9695d6e6746f0cf0dc0071bf
rinsuki <428rinsuki+git@gmail.com>
2019-07-24 14:56:55 +0900

    add ExtendUserNameLengthLimitTo50 migration

---
+++ src/db/migrations/1563947120001-ExtendUserNameLengthLimitTo50.ts
@@ -0,0 +1,9 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm"
+
+export class ExtendUserNameLengthLimitTo501563947120001 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query("ALTER TABLE users ALTER COLUMN name TYPE varchar(50)")
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {}
+}
