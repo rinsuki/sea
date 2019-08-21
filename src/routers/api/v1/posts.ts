9a1f769dfda5354974efa7942654b87e8528ee6f
96a86c <96a86c@googlegroups.com>
2019-08-21 19:51:55 +0900

    Revert "add notification cond"
    This reverts commit 1be17f37f9f2db2592ac47487f6b6bd330144433.

--- src/routers/api/v1/posts.ts
+++ src/routers/api/v1/posts.ts
@@ -13,7 +13,6 @@ import webpush from "web-push"
 import { WP_OPTIONS } from "../../../config"
 import { AlbumFileRepository } from "../../../db/repositories/albumFile"
 import { ApplicationRepository } from "../../../db/repositories/application"
-import { Application } from "../../../db/entities/application"
 
 const router = new APIRouter()
 
@@ -83,7 +82,7 @@ router.post("/", koaBody(), async ctx => {
         const subscriptions = await getRepository(Subscription)
             .createQueryBuilder("subscription")
             .where("subscription.revokedAt IS NULL")
-            .innerJoinAndSelect("subscription.user", "users")
+            .innerJoin("subscription.user", "users")
             .andWhere("users.screenName = ANY(:lusers)", { lusers: replies })
             .getMany()
         const payload = {
@@ -100,15 +99,7 @@ router.post("/", koaBody(), async ctx => {
             },
             type: "mention",
         }
-        const application = await getRepository(Application).findOne({
-            where: { id: post.application.id },
-            relations: ["ownerUser"],
-        })
         subscriptions.map(async subscription => {
-            if (post.application.isAutomated && application && application.ownerUser.id != subscription.user.id) {
-                return
-            }
-
             try {
                 const subscriptionOptions = {
                     endpoint: subscription.endpoint,
