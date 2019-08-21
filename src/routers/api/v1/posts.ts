278bd4efb2ba91ce4b61fa304fa7f3d81434ce86
96a86c <96a86c@googlegroups.com>
2019-08-21 20:30:53 +0900

    use 3eq

--- src/routers/api/v1/posts.ts
+++ src/routers/api/v1/posts.ts
@@ -67,7 +67,7 @@ router.post("/", koaBody(), async ctx => {
     publishRedisConnection.publish("timelines:public", post.id.toString())
     console.log(post.files)
     const notify = body.notify || (post.application.isAutomated ? "none" : "send")
-    if (notify == "send") {
+    if (notify === "send") {
         const now = new Date()
         const replies = Array.from(new Set(post.text.match(repliesRegex))).map(reply => reply.replace("@", ""))
         if (0 < replies.length) {
