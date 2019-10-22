105358da3f466e7c1dd2e88f67d60a11b6472aa1
rinsuki <428rinsuki+git@gmail.com>
2019-10-22 21:07:20 +0900

    らんだむらんだむ

--- src/routers/web/applications.ts
+++ src/routers/web/applications.ts
@@ -2,17 +2,18 @@ import Router = require("koa-router")
 import { WebRouterState, WebRouterCustom } from "."
 import { getRepository, Not, IsNull } from "typeorm"
 import { Application } from "../../db/entities/application"
+import { User } from "../../db/entities/user"
 
 const router = new Router<WebRouterState, WebRouterCustom>()
 
 router.get("/", async ctx => {
-    const apps = await getRepository(Application).find({
-        where: {
-            isPublic: true,
-            url: Not(IsNull),
-        },
-        relations: ["ownerUser"],
-    })
+    const apps = await getRepository(Application)
+        .createQueryBuilder("apps")
+        .leftJoinAndSelect("apps.ownerUser", "users")
+        .where("apps.isPublic = true")
+        .andWhere("apps.url IS NOT NULL")
+        .orderBy("RANDOM()")
+        .getMany()
     ctx.render("applications", { apps })
 })
 
