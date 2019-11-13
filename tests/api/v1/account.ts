c208380ad8441caa2e91d68e8572fb81ae5da3aa
rinsuki <428rinsuki+git@gmail.com>
2019-11-14 07:37:18 +0900

    fix

--- tests/api/v1/account.ts
+++ tests/api/v1/account.ts
@@ -1,8 +1,8 @@
 import { request } from "../../../src/utils/forTesting/testRequest"
 
 describe("/api/v1/account", () => {
-    test("認証がないとコケる", () => {
-        request(r => {
+    test("認証がないとコケる", async () => {
+        await request(r => {
             return r.get("/api/v1/account").expect(400)
         })
     })
