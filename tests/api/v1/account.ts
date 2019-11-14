219336a665b1faa0f4a5682ecaa6e812ea978260
rinsuki <428rinsuki+git@gmail.com>
2019-11-14 12:38:15 +0900

    fix

--- tests/api/v1/account.ts
+++ tests/api/v1/account.ts
@@ -1,18 +1,35 @@
 import { request } from "../../../src/utils/forTesting/testRequest"
 
 describe("/api/v1/account", () => {
-    test("認証がないとコケる", async () => {
+    test("認証がないなら弾く", async () => {
         await request(r =>
             r
                 .get("/api/v1/account")
                 .expect(400)
                 .expect(r => {
                     expect(r.body.errors[0].message).toBe("Please authorize")
-                    return true
                 })
         )
     })
-    test("適当な認証情報だとコケる", async () => {
+    test("変なトークンは弾く", async () => {
+        await request(r =>
+            r
+                .get("/api/v1/account")
+                .set("Authorization", "hogehoge")
+                .expect(400)
+                .expect(r => expect(r.body.errors[0].message).toBe("Invalid authorize format"))
+        )
+    })
+    test("変なトークン種別なら弾く", async () => {
+        await request(r =>
+            r
+                .get("/api/v1/account")
+                .set("Authorization", "oreore token")
+                .expect(400)
+                .expect(r => expect(r.body.errors[0].message).toBe("Authorize type is invalid"))
+        )
+    })
+    test("適当なトークンなら弾く", async () => {
         await request(r =>
             r
                 .get("/api/v1/account")
@@ -20,7 +37,6 @@ describe("/api/v1/account", () => {
                 .expect(400)
                 .expect(r => {
                     expect(r.body.errors[0].message).toBe("Authorize failed")
-                    return true
                 })
         )
     })
