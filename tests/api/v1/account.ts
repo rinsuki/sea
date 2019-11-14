583c2670cb3e063b20544cc7e0798d60913fa5f9
rinsuki <428rinsuki+git@gmail.com>
2019-11-14 12:29:09 +0900

    適当な認証情報でコケることを確認

--- tests/api/v1/account.ts
+++ tests/api/v1/account.ts
@@ -2,8 +2,26 @@ import { request } from "../../../src/utils/forTesting/testRequest"
 
 describe("/api/v1/account", () => {
     test("認証がないとコケる", async () => {
-        await request(r => {
-            return r.get("/api/v1/account").expect(400)
-        })
+        await request(r =>
+            r
+                .get("/api/v1/account")
+                .expect(400)
+                .expect(r => {
+                    expect(r.body.errors[0].message).toBe("Please authorize")
+                    return true
+                })
+        )
+    })
+    test("適当な認証情報だとコケる", async () => {
+        await request(r =>
+            r
+                .get("/api/v1/account")
+                .set("Authorization", "Bearer dummytoken")
+                .expect(400)
+                .expect(r => {
+                    expect(r.body.errors[0].message).toBe("Authorize failed")
+                    return true
+                })
+        )
     })
 })
