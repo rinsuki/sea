367831cdf841075b9b9a00a471c862ab890dd4ca
rinsuki <428rinsuki+git@gmail.com>
2020-01-16 18:32:02 +0900

    fix ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -5,13 +5,13 @@ const typeorm = require("typeorm")
 const config = {
     type: "postgres",
     url: process.env.NODE_ENV === "test" ? process.env.DATABASE_URL_TEST : process.env.DATABASE_URL,
-    entities: ["dist/db/entities/*.js"],
-    migrations: ["dist/db/migrations/*.js"],
-    subscribers: ["dist/db/subscribers/*.js"],
+    entities: ["dist/server/db/entities/*.js"],
+    migrations: ["dist/server/db/migrations/*.js"],
+    subscribers: ["dist/server/db/subscribers/*.js"],
     cli: {
-        entitiesDir: "src/db/entities/",
-        migrationsDir: "src/db/migrations/",
-        subscribersDir: "src/db/subscribers/",
+        entitiesDir: "src/server/db/entities/",
+        migrationsDir: "src/server/db/migrations/",
+        subscribersDir: "src/server/db/subscribers/",
     },
     ssl: process.env.DATABASE_SSL_ENABLED === "yes" ? true : false,
 }
