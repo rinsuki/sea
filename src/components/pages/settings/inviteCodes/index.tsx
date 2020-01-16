a254d7a48550f7fc18f4183ed0ac6f34699372ef
rinsuki <428rinsuki+git@gmail.com>
2020-01-16 21:25:07 +0900

    more elegant code

--- src/components/pages/settings/inviteCodes/index.tsx
+++ src/components/pages/settings/inviteCodes/index.tsx
@@ -4,35 +4,32 @@ import { UserSession } from "../../../../db/entities/userSession"
 import { InviteCode } from "../../../../db/entities/inviteCode"
 import { User } from "../../../../db/entities/user"
 
-export function Tree({ invites, root }: { invites: InviteCode[]; root: User }) {
-    const myInvites = invites.filter(i => i.fromUser.id === root.id && i.fromUser.id !== i.toUser?.id)
+export function Tree({ invites, root }: { invites: InviteCode[]; root?: User }) {
+    const myInvites = invites.filter(i =>
+        root ? i.fromUser.id === root.id && i.fromUser.id !== i.toUser?.id : i.fromUser.id === i.toUser?.id
+    )
     return (
         <>
-            @{root.screenName}
-            <ul>
-                {myInvites.map(i => (
-                    <li key={i.id}>
+            {myInvites.map(i => (
+                <li key={i.id}>
+                    @{i.toUser?.screenName}
+                    <ul>
                         <Tree invites={invites} root={i.toUser!} />
-                    </li>
-                ))}
-            </ul>
+                    </ul>
+                </li>
+            ))}
         </>
     )
 }
 
 export function InviteCodeIndex(props: { session: UserSession; invites: InviteCode[] }) {
-    const rootInvites = props.invites.filter(i => i.fromUser.id === i.toUser?.id)
     return (
         <Wrapper>
             <h1>招待コード</h1>
             {props.session.user.canMakeInviteCode && <a href="/settings/invite_codes/new">作成</a>}
             <h2>招待ツリー</h2>
             <ul>
-                {rootInvites.map(i => (
-                    <li key={i.id}>
-                        <Tree invites={props.invites} root={i.toUser!} />
-                    </li>
-                ))}
+                <Tree invites={props.invites} />
             </ul>
         </Wrapper>
     )
