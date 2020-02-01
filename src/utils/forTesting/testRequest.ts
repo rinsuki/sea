d04a01e85c75e32ed5b642a4e61b4e6e76f1ac33
rinsuki <428rinsuki+git@gmail.com>
2020-02-02 07:31:14 +0900

    リモートサーバーにテストを実行できるように

--- src/utils/forTesting/testRequest.ts
+++ src/utils/forTesting/testRequest.ts
@@ -5,5 +5,5 @@ const databasePromise = databaseSetup()
 
 export async function request<T>(r: (r: req.SuperTest<req.Test>) => T): Promise<T> {
     await databasePromise
-    return r(req(server))
+    return r(req(process.env.SEA_REMOTE_TEST_URL || server))
 }
