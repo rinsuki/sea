a3e527c328c2ef9bdc46344a1ff756a92c56ff7e
rinsuki <428rinsuki+git@gmail.com>
2020-11-09 14:43:35 +0900

    E2Eテスト追加 (#974)
    * wip
    
    * fix
    
    * merge to build-and-test.yaml
    
    * fix
    
    * add docker load
    
    * use prebuilt image for e2e
    
    * build phase で postgres を立てる必要はない
    
    * use tar.gz
    
    * fix
    
    * e2e: show curl response and logs
    
    * test: postgresを時間かかる処理の前に起動させておく
    
    * e2e: postgresとredisを事前に起動させておく
    
    * e2e: migration忘れてた
    
    * e2e: add wait-for-up.sh
    
    * wait-for-upは1分で死ぬように、failしてもdockerのlogを吐くように
    
    * fix
    
    * fix
    
    * fix?
    
    * e2e: wait-for-up の前にyarn install しておく
    
    * e2e: remove NODE_ENV
    
    * e2e: set NODE_ENV to e2e
    
    * e2e: add run.ts
    
    * e2e: add typescript
    
    * add @types/node
    
    * e2e: ts → js (with ts-check)
    
    * fix
    
    * error.png
    
    * fix
    
    * e2e: click input invite code link
    
    * e2e: remove unneed focus
    
    * e2e: add case
    
    * e2e: use macos runner
    
    * fix?
    
    * e2e: use windows instead of macos
    
    * e2e: okay we must use linux
    
    * e2e: fix?
    
    * e2e: add content validation
    
    * e2e: fix

---
+++ e2e/run.mjs
@@ -0,0 +1,63 @@
+// @ts-check
+
+import { firefox } from "playwright-firefox"
+import got from "got"
+
+async function main() {
+    const browser = await firefox.launch()
+    const page = await browser.newPage()
+    try {
+        // register
+        await page.goto("http://localhost:3000")
+        await page.click(`a[href="/register"]`)
+        await page.type(`input[name="name"]`, "admin")
+        await page.type(`input[name="screen_name"]`, "admin")
+        await page.type(`input[name="password"]`, "adminadmin")
+        await page.click(`input[value="登録"]`)
+        await page.click(`a[href="/input_invite_code"]`)
+        await page.click(`input[value="送信"]`)
+        // register app
+        await page.click(`a[href="/settings"]`)
+        await page.click(`a[href="/settings/my_developed_applications"]`)
+        await page.click(`a[href="/settings/my_developed_applications/new"]`)
+        await page.type(`input[name="name"]`, "e2etester")
+        await page.type(`textarea[name="description"]`, "E2E Tester")
+        await page.click(`input[value="アプリを作成"]`)
+        // generate token
+        await Promise.all([
+            page.waitForEvent("dialog").then(dialog => {
+                if (dialog.message() !== "アクセストークン発行モードに突入!!") throw `${dialog.message()}`
+                return dialog.accept()
+            }),
+            page.click(`form[action$="/my_token"] input[type="submit"]`)
+        ])
+        const token = await page.$eval(`input#access_token`, (/** @type {HTMLInputElement} */ elm) => elm.value)
+        // create post
+        const post = await got.post("http://localhost:3000/api/v1/posts", {
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                text: "this is test"
+            }),
+            responseType: "json",
+        })
+        // see post
+        await page.goto(`http://localhost:3000/posts/${post.body.id}`)
+        const text = await page.$eval(`#posts_${post.body.id} + dd`, e => e.textContent)
+        if (text !== "this is test") throw `wrong text: ${text}`
+    } finally {
+        await page.screenshot({
+            path: "screenshot.png",
+            type: "png",
+            fullPage: true,
+        })
+    }
+    await browser.close()
+}
+
+main().catch(e => {
+    console.error(e)
+    process.exit(1)
+})
