63854012dd46613213289becefa032675ccce825
rinsuki <428rinsuki+git@gmail.com>
2020-11-09 13:49:19 +0900

    e2e: add case

--- e2e/run.mjs
+++ e2e/run.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 
 import { firefox } from "playwright-firefox"
+import got from "got"
 
 async function main() {
     const browser = await firefox.launch()
-    // register
     const page = await browser.newPage()
     try {
+        // register
         await page.goto("http://localhost:3000")
         await page.click(`a[href="/register"]`)
         await page.type(`input[name="name"]`, "admin")
@@ -15,13 +16,38 @@ async function main() {
         await page.click(`input[value="登録"]`)
         await page.click(`a[href="/input_invite_code"]`)
         await page.click(`input[value="送信"]`)
-    } catch(e) {
+        // register app
+        await page.click(`a[href="/settings"]`)
+        await page.click(`a[href="/settings/my_developed_applications"]`)
+        await page.click(`a[href="/settings/my_developed_applications/new"]`)
+        await page.type(`input[name="name"]`, "e2etester")
+        await page.type(`textarea[name="description"]`, "E2E Tester")
+        await page.click(`input[value="アプリを作成"]`)
+        // generate token
+        await page.click(`form[action$="/my_token"] input[value="ボタンを押せ!!"]`)
+        const dialog = await page.waitForEvent("dialog")
+        if (dialog.message() !== "アクセストークン発行モードに突入!!") throw `${dialog.message()}`
+        await dialog.accept()
+        const token = await page.$eval(`input#access_token`, (/** @type {HTMLInputElement} */ elm) => elm.value)
+        // create post
+        const post = await got.post("http://localhost:3000/api/v1/posts", {
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                text: "this is test"
+            }),
+            responseType: "json",
+        })
+        // see post
+        await page.goto(`http://localhost:3000/posts/${post.body.id}`)
+    } finally {
         await page.screenshot({
-            path: "error.png",
+            path: "screenshot.png",
             type: "png",
             fullPage: true,
         })
-        throw e
     }
     await browser.close()
 }
